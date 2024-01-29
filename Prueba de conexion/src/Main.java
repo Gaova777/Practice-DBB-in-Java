22db0a335794e1d0d17516b0c6369a329645988e
Juan Diego Garzon Ovalle <gaova777@utp.edu.co>
2024-01-28 20:36:24 -0500

    6. Actualización y eliminacion de datos JDBC

--- Prueba de conexion/src/Main.java
+++ Prueba de conexion/src/Main.java
@@ -6,21 +6,21 @@ public class Main {
     public static void main(String[] args) {
 
         Connection myConn = null;
-        PreparedStatement myStamt = null;
-//        ResultSet myRes = null;
+//        PreparedStatement myStamt = null;
+        Statement myStamt = null;
+        ResultSet myRes = null;
         try {
             myConn= DriverManager.getConnection("jdbc:mysql://localhost:3306/project","root","");
             System.out.println("Nos CONECTAMOS");
-            String sql = "INSERT INTO employees (first_name, pa_surname) VALUES(?,?)";
-            myStamt = myConn.prepareStatement(sql);
-            myStamt.setString(1,"MOni");
-            myStamt.setString(2,"Cano");
 
-            int rowsAffected = myStamt.executeUpdate();
-//            myRes = myStamt.executeQuery("SELECT * FROM employees");
+            myStamt = myConn.createStatement();
 
-            if (rowsAffected>0){
-                System.out.println("Se ha insertado un empleado on exito");
+            int rowsAffected = myStamt.executeUpdate("UPDATE employees "+"set email='gaova7@gmail.com' "+"WHERE first_name='Juan'");
+
+            myRes = myStamt.executeQuery("SELECT * FROM employees order by pa_surname");
+
+            while (myRes.next()){
+                System.out.println(myRes.getString("first_name")+", "+myRes.getString("email"));
             }
 
         }catch (Exception e){
